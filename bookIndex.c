29b762d38612778380ef8e7c506e95907f5c6b43
kingJTao <951086498@qq.com>
2018-09-07 15:33:07 +0800

    功能完善

--- bookIndex.c
+++ bookIndex.c
@@ -1,46 +1,123 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include "HString.h"
 #include "bookIndex.h"
 
 int main(int argc,char *argv[]){
 	FILE *ff = fopen("Books.txt","r"),
-		 *gf = fopen("BookIndex.txt","w");
-	ElemType BookNo;
-	IdxListType *idxlist = (IdxListType*)malloc(sizeof(IdxListType));
+		 *gf = fopen("bookIndex.txt","w");
+	HString BookNo;
+	IdxListType *idxlist;
 
 	if(ff && gf && idxlist){
-		InitIdxList(idxlist);
+		InitIdxList(&idxlist);
 
 		while( !feof(ff) ){
 			GetLine(ff);
 			ExtractKeyWord(&BookNo);
-			InsIdxList(idxlist, BookNo);
+			InsIdxList(idxlist,BookNo);
 		}
 		PutText(gf, idxlist);
+		//关闭文件
+		fclose(ff);
+		fclose(gf);
 	}
 }
 
-void InitIdxList(IdxListType *idxlist){
+void InitIdxList(IdxListType **idxlist){
+	*idxlist = (IdxListType*)malloc(sizeof(IdxListType));
+
+	if( *idxlist ){
+		(*idxlist)->last = 0;
+	}
+}
+
+void InitList(LinkList *bnostruct){
+	LinkList temp = (LinkList)malloc(sizeof(sBookNo));
+
+	if(temp){
+		temp->next = NULL;
+		*bnostruct = temp;
+	}
+}
+
+void tolowerFn(char *chars){
+	size_t clen = strlen(chars);
+
+	for(size_t i = 0; i < clen; ++i){
+		if(isalpha(chars[i]) && isupper(chars[i])){
+			chars[i] = tolower(chars[i]);
+		}
+	}
+}
+
+int iskeyword(char *chars){
+	int keyword = 1;
+	String temp = strtoString(chars);
+
+	if(Index(nokeyword,temp,0)>=0){
+		keyword = 0;
+	}
+	
+	return keyword;
 }
 
 void Append(LinkList collect,LinkList p){
+	LinkList first = collect;
+
+	for( ; first && first->next; first = first->next);
+	first->next = p;
 }
 
 void GetLine(FILE *f){
+	_IO_ssize_t read;
+	size_t readin = 0;
+
+	read = getline(&buf, &readin, f);
+	if( read!=-1 && read>=1 ){
+		buf[read-1] = '\0';
+	}
 }
 
-void ExtractKeyWord(ElemType *bn){
+void ExtractKeyWord(HString *bn){
+	char *bresult = strtok(buf," ");
+	
+	if( bresult ){
+		int i;
+		
+		*bn = (HString)malloc(sizeof(String));
+		if( bn ){
+			StrAssign(*bn,bresult);
+			bresult = strtok(NULL," ");
+			while( bresult ){
+				tolowerFn(bresult);
+				if( iskeyword(bresult) ){
+					for(i = 0; i < wdlist.last; ++i){
+						if( strcmp(bresult,wdlist.item[i])==0 ){
+							break;
+						}
+					}
+					if(i==wdlist.last){
+						wdlist.item[i] = bresult;
+						wdlist.last++;
+					}
+				}
+				bresult = strtok(NULL," ");
+			}
+		}
+	}
 }
 
-int InsIdxList(IdxListType *idxlist,ElemType bno){
+int InsIdxList(IdxListType *idxlist,HString bno){
 	HString wd = (HString)malloc(sizeof(String));
 	int result = 0;
 
 	if(wd){
 		for(int i = 0; i < wdlist.last; ++i){
 			GetWord(i, wd);
-			j = Locate(idxlist, wd, &result);
+			int j = Locate(*idxlist, wd, &result);
 			if( !result ){
 				InsertNewKey(idxlist, j, wd);					//插入新的索引项
 			}else{
@@ -52,7 +129,22 @@ int InsIdxList(IdxListType *idxlist,ElemType bno){
 	return result;
 }
 
-void PutText(FILE *g,IdxListType idxlist){
+void PutText(FILE *g,IdxListType *idxlist){
+	LinkList temp;
+
+	for(int i = 0; i < idxlist->last; ++i){
+		fputs(idxlist->item[i].key->chars,g);
+		fputc(' ', g);
+		temp = idxlist->item[i].bnolist->next;
+		while( temp ){
+			fputs(temp->bookno->chars, g);
+			temp = temp->next;
+			if(temp){
+				fputc(',', g);
+			}
+		}
+		fputc('\n', g);
+	}
 }
 
 void GetWord(int i,HString wd){
@@ -61,10 +153,10 @@ void GetWord(int i,HString wd){
 }
 
 int Locate(IdxListType idxlist,HString wd,int *b){
-	int m,
+	int m,i,
 		result;
 
-	for(int i = idxlist.last - 1; (m = StrCompare(idxlist.item[i].key, wd)) > 0; --i);
+	for(i = idxlist.last - 1; (m = StrCompare(*(idxlist.item[i].key), *wd)) > 0; --i);
 	if(m == 0){
 		*b = 1;
 		result = i;
@@ -77,23 +169,24 @@ int Locate(IdxListType idxlist,HString wd,int *b){
 }
 
 void InsertNewKey(IdxListType *idxlist,int i,HString wd){
-	for(int j = idxlist.last - 1; j >= i; --j){//后移索引项
-		idxlist.item[j+1] = idxlist.item[j];
+	for(int j = idxlist->last - 1; j >= i; --j){//后移索引项
+		idxlist->item[j+1] = idxlist->item[j];
 	}
 	//插入新的索引项
-	StrCopy(idxlist.item[i].key, wd);
-	InitList(idxlist.item[i].bnolist);
-	++idxlist.last;
+	StrCopy(idxlist->item[i].key, *wd);
+	InitList(&(idxlist->item[i].bnolist));
+	++idxlist->last;
 }
 
-int InsertBook(IdxListType *idxlist,int i,int bno){
+int InsertBook(IdxListType *idxlist,int i,HString bno){
 	LinkList p = (LinkList)malloc(sizeof(sBookNo));
 
 	if(!p){
 		return 0;
 	}else{
 		p->bookno = bno;
-		Append(idxlist.item[i].bnolist,p);
+		p->next = NULL;
+		Append(idxlist->item[i].bnolist,p);
 		return 1;
 	}
 }
--- bookIndex.h
+++ bookIndex.h
@@ -1,17 +1,17 @@
 #define MaxBookNum 1000				// 假设只对1000本书建索引表
 #define MaxKeyNum 2500				// 索引表的最大容量
 #define MaxLineLen 500				// 书目串的最大长度
-#define MaxWordNum 10				// 词表的最大容量
+#define MaxWordNum 100				// 词表的最大容量
 
 typedef struct{
-	char *item[];		// 字符串的数组
-	int last;			// 词表的长度
-}WordListType;			// 词表类型(顺序表)
+	char *item[MaxWordNum];		// 字符串的数组
+	int last;					// 词表的长度
+}WordListType;					// 词表类型(顺序表)
 
 typedef int ElemType;	//定义连表的数据元素类型为整形(书号类型)
 
 typedef struct bookno{
-	ElemType bookno;
+	HString bookno;
 	struct bookno *next;
 }*LinkList,sBookNo;
 
@@ -28,10 +28,20 @@ typedef struct{
 //主要变量
 char *buf;				//书目串缓冲区
 WordListType wdlist;	//词表
+String nokeyword = {"a/an/in/on/of/off/the/and\0",25};
 
 /*******************基本操作************************/
 //初始化操作，置索引表idexlist为空表，且在idxlist.item[0]设一空串
-void InitIdxList(IdxListType *idxlist);
+void InitIdxList(IdxListType **idxlist);
+
+//初始化书号链表
+void InitList(LinkList *bnostruct);
+
+//大写转换为小写
+void tolowerFn(char *chars);
+
+//判断是否为关键词
+int iskeyword(char *chars);
 
 //插入书号链表
 void Append(LinkList collect,LinkList p);
@@ -40,13 +50,13 @@ void Append(LinkList collect,LinkList p);
 void GetLine(FILE *f);
 
 //从buf中提取书名关键词到词表wdlist，书号存入bno
-void ExtractKeyWord(ElemType *bn);
+void ExtractKeyWord(HString *bn);
 
 //将书号为bno的书名关键词按辞典顺序插入索引表idxlist
-int InsIdxList(IdxListType *idxlist,ElemType bno);
+int InsIdxList(IdxListType *idxlist,HString bno);
 
 //将生成的索引表idxlist输出到文件g
-void PutText(FILE *g,IdxListType idxlist);
+void PutText(FILE *g,IdxListType *idxlist);
 
 //用wd返回词表wdlist中第i个关键词
 void GetWord(int i,HString wd);
@@ -59,4 +69,4 @@ int Locate(IdxListType idxlist,HString wd,int *b);
 void InsertNewKey(IdxListType *idxlist,int i,HString wd);
 
 //在索引表idxlist的第i项中插入书号为bno的索引
-int InsertBook(IdxListType *idxlist,int i,int bno);
+int InsertBook(IdxListType *idxlist,int i,HString bno);
